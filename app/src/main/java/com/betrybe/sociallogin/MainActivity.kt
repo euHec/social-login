a73ebcf27d0c1896148d05c09fcf47e594865825
Hector Souza <hectorsouzass@gmail.com>
2024-04-05 04:02:08 -0300

    Requisito 7, 8, 9, 10

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.betrybe.sociallogin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
-
+    private val email_input by lazy { findViewById<TextInputLayout>(R.id.email_text_input_layout)}
+    private val password_input by lazy { findViewById<TextInputLayout>(R.id.password_text_input_layout) }
+    private val button by lazy { findViewById<Button>(R.id.login_button) }
+    private fun validarEmail(email: Editable): Boolean {
+        val regex = Regex("[A-Za-z0-9._%+-]+@[A-Za-z]+\\.[A-Za-z]{2,}")
+        return regex.matches(email)
+    }
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val email = email_input.editText!!
+        val password = password_input.editText!!
+
+        email.addTextChangedListener {
+            button.isEnabled = email.text.isNotEmpty() && password.text.isNotEmpty()
+        }
+
+        password.addTextChangedListener {
+            button.isEnabled = email.text.isNotEmpty() && password.text.isNotEmpty()
+        }
+
+        button.setOnClickListener {
+            val isTrue = validarEmail(email.text)
+            val isValidPassword = password.text.length < 4
+            if (!isTrue) email.error = getString(R.string.email_warning)
+            if (isValidPassword) password.error = getString(R.string.password_warning)
+            if (isTrue && !isValidPassword) {
+                Snackbar.make(findViewById(android.R.id.content), R.string.login_succeeded, Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 }
